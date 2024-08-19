ca96a0d939f1bd1061fe31031b18d87bd8e98e2c
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-08-18 18:11:50 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+var form = document.querySelector(".hero form");
+var input = document.querySelector("#dog-name");
+var headerText = document.querySelector(".hero h1 strong");
+
+
+form.addEventListener("submit", displayDogName);
+
+function displayDogName(event) {
+  event.preventDefault();
+  var dogName = input.value;
+  headerText.innerText = dogName;
+};
+
+// form.addEventListener("submit", function (event){
+//     event.preventDefault();
+//     var dogName = input.ariaValueMax;
+//     headerText.innerText = dogName;
+// });
+
+console.log(form)
+console.log(input)
+console.log(headerText)
